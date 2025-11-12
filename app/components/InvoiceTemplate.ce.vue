778875b25cf616d082657d20f32a7bc551b1e8f4
wdrf14 <weidermartenn@gmail.com>
2025-11-12 15:22:20 +0300

    asdasd

--- app/components/InvoiceTemplate.ce.vue
+++ app/components/InvoiceTemplate.ce.vue
@@ -5,7 +5,6 @@
         <h1><span class="hide-in-print">INTERACTIVE </span>INVOICE</h1>
         <div class="hide-in-print" style="margin-bottom:10px">This page is rendered from a "template" Vue.js component.<br>You can modify the fields highlighted in green and interact with the buttons without having them in the print. Check out <a href="https://github.com/motla/vue-document-editor/blob/master/src/Demo/InvoiceTemplate.ce.vue" target="_blank">InvoiceTemplate.ce.vue</a></div>
       </div>
-      <div><img src="../../img/vue-logo.png" style="width:80px"></div>
     </div>
     <table>
       <thead>
